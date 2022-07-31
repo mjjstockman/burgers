82fed6b9d54a4c5ff347841c38c9091c3b48ac08
mjjstockman <mjjstockman@gmail.com>
2022-07-31 11:29:25 +0000

    Add user_confirm() to ask user to confirm data entry

--- run.py
+++ run.py
@@ -26,8 +26,54 @@ def show_burgers():
     choose_burger()
 
 def choose_burger():
-    print("Please choose which burger you would like\n")
-    burger_choice = input("Enter your choice below\n")
-    print(f"You entered {burger_choice}")
+    while True:
+        print("Please choose which burger you would like\n")
+        burger_choice_num = input("Enter your choice below\n")
+        if validate_burger_choice(burger_choice_num):
+            if user_confirm(burger_choice_num):
+                print(f"You chose {burger_choice_num}, from 33")
+                break
+        else:
+            choose_burger()
+
+def validate_burger_choice(burger_choice_num):
+    try:
+        if int(burger_choice_num) not in {1, 2, 3, 4}:
+            raise ValueError(
+                "Must be a whole num between 1 and 4"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again")
+        return False
+
+    return True
+
+def user_confirm(data):
+    print(f"You entered {data}\n")
+    print("Is this correct?\n")
+    confirm = input("Enter Y for yes, N for No:\n")
+    confirm_strip_lcase = confirm.strip().lower()
+    if confirm_strip_lcase == "y":
+        print(f"You said {data} is correct!")
+        return True
+    elif confirm_strip_lcase == "n":
+        print("Let's try again")
+        return False
+    else:
+        print("Input must be either a Y or N")
+        user_confirm(data)
+
+    # if burger_choice_num == 1:
+    #     burger = "Beef"
+    # elif burger_choice_num == 2:
+    #     burger = "Chicken"
+    #     print(f"You entered {burger}")
+    # elif burger_choice_num == 3:
+    #     burger = "Vegan"
+    # elif burger_choice_num == 4:
+    #     burger = "Tofu"
+    #     print(f"You chose {burger}")
+
+    
 
 welcome()
