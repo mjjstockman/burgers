360804b90cdf4d921aa908875fb3c9891a853ea3
mjjstockman <mjjstockman@gmail.com>
2022-08-02 09:50:02 +0000

    Add docstrings

--- README.md
+++ README.md
@@ -12,19 +12,88 @@ They want an app made where guests at the party can choose the food and drink th
  1. Welcomes the user 
  2. Displays the choice of burgers (from the burgers sheet)
  3. Asks the user to choose a burger type
- 4. Asks the user to confirm their choice of burger
- 5. Displays the choice of fries (from the fries sheet)
- 6. Asks the user to choose a type of fries
- 7. Asks the user to confirm their choice of burger
- 8. Displays the choice of drinks (from the drinks sheet)
- 9. Asks the user to choose a drink type
- 10.  Asks the user to confirm their choice of drink
- 11. Asks the user if they would like to to add whisky to their drink
- 12. If they want whisky they are asked for their date of birth
- 13. If they are under 18 they are informed they are too young
- 14. The order is confirmed to the user
- 15. The order is added to the order worksheet
+ 4. The choice is validated to be a correct choice
+ 5. Asks the user to confirm their choice of burger
+ 6. Displays the choice of fries (from the fries sheet)
+ 7. Asks the user to choose a type of fries
+ 8. Asks the user to confirm their choice of fries
+ 9. Displays the choice of drinks (from the drinks sheet)
+ 10. Asks the user to choose a drink type
+ 11.  Asks the user to confirm their choice of drink
+ 12. Asks the user if they would like to to add whisky to their drink
+ 13. If they want whisky they are asked for their date of birth
+ 14. If they are under 18 they are informed they are too young
+ 15. The order is confirmed to the user
+ 16. The order is added to the order worksheet
 
 # Testing
 
+The main areas for testing have been carried out manually and evidenced in the images below:
+
  1. The user is welcomed
+<p><img src="docs/testing_images/welcome.png"></p>
+	
+2. The choice of burgers are displayed
+<p><img src="docs/testing_images/burger-choice.png"></p>
+
+ These are taken from the Burgers sheet
+<p><img src="docs/testing_images/burgers-ws.png"></p>
+ 
+ 3. The user is asked to choose a burger type
+ <p><img src="docs/testing_images/choose-burger.png"></p>
+
+4. If the user choice is not a valid choice the user is prompted to try again
+ <p><img src="docs/testing_images/invalid-user-choice.png"></p>
+
+5. Confirming burger choice
+ <p><img src="docs/testing_images/confirm-burger.png"></p>
+
+Whenever confirming a choice only a user input of Y, y, N or n is accepted
+
+Entering an incorrect number
+<p><img src="docs/testing_images/num-confirm-error.png"></p>
+Entering an incorrect character
+<p><img src="docs/testing_images/char-confirm-error.png"></p>
+Entering an uppercase Y
+<p><img src="docs/testing_images/confirm-upper-y.png"></p>
+Entering a lowercase y
+<p><img src="docs/testing_images/confirm-lower-y.png"></p>
+Entering an uppercase N
+<p><img src="docs/testing_images/confirm-upper-n.png"></p>
+Entering a lowercase n
+<p><img src="docs/testing_images/confirm-lower-n.png"></p>
+
+
+6. The choice of fries are displayed
+<p><img src="docs/testing_images/fries-choice.png"></p>
+
+These are taken from the fries sheet
+<p><img src="docs/testing_images/fries-sheet.png"></p>
+
+7. The user is asked to choose a type of fry
+<p><img src="docs/testing_images/choose-fries.png"></p>
+
+8. If the user choice is not a valid choice the user is prompted to try again
+
+9. Displays the choice of drinks (from the drinks sheet)
+<p><img src="docs/testing_images/choose-fries.png"></p>
+
+10.  Asks the user to confirm their choice of drink
+<p><img src="docs/testing_images/show-drinks-and-ask.png"></p>
+ 
+11. Asks the user if they would like to to add whisky to their drink
+<p><img src="docs/testing_images/whisky.png"></p>
+ 
+12. If they want whisky they are asked for their date of birth, which must be in the correct format
+<p><img src="docs/testing_images/validate-date.png"></p>
+
+13. If they are under 18 they are informed they are too young
+<p><img src="docs/testing_images/u18.png"></p>
+ 
+15. The order is relayed to the user
+<p><img src="docs/testing_images/order.png"></p>
+
+16. The order is added to the order worksheet
+<p><img src="docs/testing_images/order-with-whisky.png"></p>
+
+
--- run.py
+++ run.py
@@ -27,11 +27,16 @@ items = []
 
 
 def welcome():
+    """welcomes the user with a simple greeting
+    """
     print("Welcome to Burgers!\n")
     show_burgers()
 
 
 def show_burgers():
+    """Uses tabulate to pretty print the burgers
+    Calls the choose_burger function
+    """
     print("Here are our burgers...\n")
     print(tabulate(burger_data, headers="firstrow", showindex="always",
           tablefmt="fancy_grid"))
@@ -39,6 +44,14 @@ def show_burgers():
 
 
 def choose_burger():
+    """a loop to continue asking the user to choose a burger until their choice 
+    is validated
+    The user is then asked to confirm their choice
+    If validated and confirmed the choice is added to the items list and 
+    show_fries is called.
+    If the user does not confirm their choice or enter an invalid choice the
+    function calls itself again
+    """
     while True:
         print("Please choose which burger you would like\n")
         burger_choice_num = input("Enter your choice below\n")
@@ -59,6 +72,14 @@ def choose_burger():
             choose_burger()
 
 def choose_drink():
+    """a loop to continue asking the user to choose a drink until their choice 
+    is validated
+    The user is then asked to confirm their choice
+    If validated and confirmed the choice is added to the items list and 
+    add_shot is called.
+    If the user does not confirm their choice or enter an invalid choice the
+    function calls itself again
+    """
     while True:
         print("Please choose which drink you would like\n")
         drink_choice_num = input("Enter your choice below\n")
@@ -78,10 +99,22 @@ def choose_drink():
             
 
 def validate_burger_choice(burger_choice_num):
+    """If the entered number is not an int between 0 and 3
+    an exception is raised
+
+    Args:
+        burger_choice_num (str): the users choice of burger
+
+    Raises:
+        ValueError: when an invalid choice is made
+
+    Returns:
+        True if there are no errors
+    """
     try:
         if int(burger_choice_num) not in {0, 1, 2, 3}:
             raise ValueError(
-                "Must be a whole num between 1 and 4"
+                "Must be a whole num between 0 and 3"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again")
@@ -90,6 +123,18 @@ def validate_burger_choice(burger_choice_num):
 
 
 def validate_fries_choice(fries_choice_num):
+    """If the entered number is not an int between 0 and 2
+    an exception is raised
+
+    Args:
+        fries_choice_num (str): the users choice of fries
+
+    Raises:
+        ValueError: when an invalid choice is made
+
+    Returns:
+        True if there are no errors
+    """
     try:
         if int(fries_choice_num) not in {0, 1, 2}:
             raise ValueError(
@@ -102,6 +147,18 @@ def validate_fries_choice(fries_choice_num):
 
 
 def user_confirm(data):
+    """asks the user to confirm a choice by entering Y, y, N or n
+    THe answer is stripped of whitespace and if the user does not enter a 
+    valid answer they are asked again
+
+
+    Args:
+        data (str): the data the user is confirming
+
+    Returns:
+        boolean: True if user eenters Y or y
+        boolean: False if user eenters N or n
+    """
     print(f"You entered {data}\n")
     print("Is this correct?\n")
     confirm = input("Enter Y for yes, N for No:\n")
@@ -118,6 +175,9 @@ def user_confirm(data):
 
 
 def show_fries():
+    """Uses tabulate to pretty print the fries
+    Calls the choose_fries function
+    """
     print("\nHere are our fries...\n")
     print(tabulate(fries_data, headers="firstrow", showindex="always",
           tablefmt="fancy_grid"))
@@ -125,6 +185,14 @@ def show_fries():
 
 
 def choose_fries():
+    """a loop to continue asking the user to choose their fries until their
+    choice is validated
+    The user is then asked to confirm their choice
+    If validated and confirmed the choice is added to the items list and 
+    show_drinks is called.
+    If the user does not confirm their choice or enter an invalid choice the
+    function calls itself again
+    """
     while True:
         print("Please choose which fries you would like\n")
         fries_choice_num = input("Enter your choice below\n")
@@ -144,6 +212,9 @@ def choose_fries():
 
 
 def show_drinks():
+    """Uses tabulate to pretty print the drinks
+    Calls the choose_drink function
+    """
     print("Here are our drinks...\n")
     print(tabulate(drinks_data, headers="firstrow", showindex="always",
           tablefmt="fancy_grid"))
@@ -151,6 +222,14 @@ def show_drinks():
 
 
 def choose_drink():
+    """a loop to continue asking the user to choose their drink until their
+    choice is validated
+    The user is then asked to confirm their choice
+    If validated and confirmed the choice is added to the items list and 
+    add_whisky is called.
+    If the user does not confirm their choice or enter an invalid choice the
+    function calls itself again
+    """
     while True:
         print("Please choose which drink you would like\n")
         drink_choice_num = input("Enter your choice below\n")
@@ -170,6 +249,18 @@ def choose_drink():
 
 
 def validate_drink_choice(drink_choice_num):
+     """If the entered number is not an int between 0 and 2
+    an exception is raised
+
+    Args:
+        drinks_choice_num (str): the users choice of drink
+
+    Raises:
+        ValueError: when an invalid choice is made
+
+    Returns:
+        True if there are no errors
+    """
     try:
         if int(drink_choice_num) not in {0, 1, 2}:
             raise ValueError(
@@ -182,6 +273,14 @@ def validate_drink_choice(drink_choice_num):
 
 
 def add_whisky(drink_choice_num):
+    """Asks the user if they want a shot of whisky in their drink
+
+    Args:
+        drink_choice_num (str): _description_
+
+    Returns:
+        True if there are no errors
+    """
     while True:
         add_shot = input(f"\nAdd a shot of whisky to your drink?\n")
         add_shot_strip_lcase = add_shot.strip().lower()
@@ -197,6 +296,9 @@ def add_whisky(drink_choice_num):
 
 
 def get_dob():
+    """asks the user for their date of birth if ordering whisky
+    Adss whisky to the item list if so
+    """
     print("\nPlease enter your date of birth\n")
     print("This should be in the dd/mm/yy format")
     dob = input("For example: 15/12/90\n")
@@ -213,6 +315,14 @@ def get_dob():
 
 
 def check_age(dob):
+    """Checks if the user is 18 or above
+
+    Args:
+        dob (datetime): users date of birth
+
+    Returns:
+        boolean: True if over 18, False if not
+    """
     today = datetime.datetime.today()
     dob_date = datetime.datetime.strptime(dob, "%d/%m/%y")
     age = today.year - dob_date.year - ((today.month, today.day) <
@@ -224,6 +334,14 @@ def check_age(dob):
 
 
 def validate_dob_format(dob):
+    """validates date of birth format to be d/m/y
+
+    Args:
+        dob (datetime): users date of birth
+
+    Returns:
+        boolean: True if date of birth is validated
+    """
     try:
         datetime.datetime.strptime(dob, '%d/%m/%y')
         return True
@@ -231,21 +349,17 @@ def validate_dob_format(dob):
         print("let's try again!")
 
 
-def review_order():
-    print("You have ordered....")
-
-
-def calulate_age(dob):
-    print("Calculating dob on 216")
-
-
 def show_order():
+    """Shows the order back to the customer
+    """
     print("You have ordered the following:\n")
     print(f"Burger number {items[0]}, number {items[1]} fries and number {items[2]} drink")
     send_to_order()
 
 
 def send_to_order():
+    """Adds the order to the orders worksheet
+    """
     order_worksheet = SHEET.worksheet("orders")
     order_worksheet.append_row(items)
 
